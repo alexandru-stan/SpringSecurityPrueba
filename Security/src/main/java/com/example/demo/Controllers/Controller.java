6533fd3c265742d62d76fef40dbf54388b4f0ab9
Alexandru Stan <alexxst56@gmail.com>
2023-09-13 16:09:48 +0200

    ADD SECURITY FILTERS & BASIC AUTH

--- Security/src/main/java/com/example/demo/Controllers/Controller.java
+++ Security/src/main/java/com/example/demo/Controllers/Controller.java
@@ -1,5 +1,6 @@
 package com.example.demo.Controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,17 @@ public class Controller {
 	@GetMapping("/home")
 	public String HelloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping("/mod")
+	@PreAuthorize("MOD")
+	public String Mod() {
+		return "OK, you are a mod";
+	}
+	
+	@GetMapping("/admin")
+	public String Admin() {
+		return "OK, you are an admin";
 	}
 	
 	
--- Security/src/main/java/com/example/demo/Security/ApplicationSecurityConfig.java
+++ Security/src/main/java/com/example/demo/Security/ApplicationSecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,28 +20,63 @@ public class ApplicationSecurityConfig {
 
 	
 	//BASIC AUTH
+	// SE NECESITA AUTENTICACIÓN BASIC
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((e)->{
-			e
+		
+		http.authorizeHttpRequests(e->{
+			e.requestMatchers("/home").permitAll()
 			.anyRequest()
 			.authenticated();
-		}).httpBasic(withDefaults());			
-		return http.build();
-	}
+		}).httpBasic(withDefaults());
 	
-//	
+			
+		
+		
+		return http.build();
+	}	
+	
+
+	
+	
+	
+	
+// PERMITE EL ACCESO A CUALQUIER USUARIO SIN NECESIDAD DE AUTENTICARSE
 //	@Bean
 //	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //		http.authorizeHttpRequests((e)->{
 //			e
 //			.anyRequest().permitAll();
-//		}).httpBasic(withDefaults());			
+//		});			
 //		return http.build();
 //	}
 	
 	
 	
+	
+	@Bean 
+	public UserDetailsService userDetails(PasswordEncoder encoder) {
+		
+		
+		UserDetails user = User.withUsername("alex")
+						.password(encoder.encode("stan"))
+						.roles("USER")
+						.build();
+		
+		UserDetails mod = User.withUsername("mod")
+				.password(encoder.encode("mod"))
+				.roles("MOD")
+				.build();
+		
+	return new InMemoryUserDetailsManager(user,mod);
+		
+	}
+	
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 	
 	
